e8819cd37e6bb7b3bf0511184bdb4d741d0d9e28
정민혁 <jlssteeve1@gmail.com>
2022-01-27 19:22:59 +0900

    Add explain

--- app/src/main/java/com/app/dudda/data/music/MusicService.kt
+++ app/src/main/java/com/app/dudda/data/music/MusicService.kt
@@ -4,6 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface MusicService {
+    // mocky에서 만든 임시 api, 음악을 불러오기 위해 사용
     @GET("/v3/bf57b160-d50f-4946-be5d-d40be9090743")
     fun listMusics() : Call<MusicDTO>
 }
