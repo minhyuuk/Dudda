45c716026d3470b3c8db55ce10bbbd8347a98cf1
정민혁 <jlssteeve1@gmail.com>
2022-02-05 17:12:22 +0900

    Add music dto mapping

--- app/src/main/java/com/app/dudda/data/music/MusicModelMapping.kt
+++ app/src/main/java/com/app/dudda/data/music/MusicModelMapping.kt
@@ -1,6 +1,6 @@
 package com.app.dudda.data.music
 
-fun MusicEntity.mapper(id: Long) : MusicModel =
+fun MusicEntity.mapper(id: Long): MusicModel =
     MusicModel(
         id = id,
         streamUrl = this.streamUrl,
@@ -8,4 +8,14 @@ fun MusicEntity.mapper(id: Long) : MusicModel =
         track = this.track,
         artistName = artistName
     )
+
+fun MusicDTO.mapper(): PlayerModel =
+    PlayerModel(
+        playMusicList = this.musics.mapIndexed { index, musicEntity ->
+            // mapper를 가지고 있음. MusicModelMapping 참고
+            // 반환값은 MusicModel
+            musicEntity.mapper(index.toLong())
+        }
+    )
+
 
