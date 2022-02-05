9d78cf9888e7e2db1e189a92f7dd4a1061cbe799
정민혁 <jlssteeve1@gmail.com>
2022-02-05 17:46:17 +0900

    Add next music, previous music play

--- app/src/main/java/com/app/dudda/data/music/PlayerModel.kt
+++ app/src/main/java/com/app/dudda/data/music/PlayerModel.kt
@@ -14,5 +14,23 @@ data class PlayerModel (
             newItem
         }
     }
+
+    fun updateCurrentPosition(musicModel: MusicModel) {
+        currentPosition = playMusicList.indexOf(musicModel)
+    }
+
+    fun nextMusic(): MusicModel? {
+        if(playMusicList.isEmpty()) return null
+
+        currentPosition = if((currentPosition+1) == playMusicList.size) 0 else currentPosition + 1
+        return playMusicList[currentPosition]
+    }
+
+    fun previousMusic(): MusicModel? {
+        if(playMusicList.isEmpty()) return null
+
+        currentPosition = if((currentPosition -1) < 0) playMusicList.lastIndex else currentPosition - 1
+        return playMusicList[currentPosition]
+    }
 }
 
