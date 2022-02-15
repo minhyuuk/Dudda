325d3da63254412b5233af9913c33e1e62a1d840
minhyuuk <jlssteeve1@gmail.com>
2022-02-15 22:03:49 +0900

    Add current music model

--- app/src/main/java/com/app/dudda/data/music/PlayerModel.kt
+++ app/src/main/java/com/app/dudda/data/music/PlayerModel.kt
@@ -32,5 +32,10 @@ data class PlayerModel (
         currentPosition = if((currentPosition -1) < 0) playMusicList.lastIndex else currentPosition - 1
         return playMusicList[currentPosition]
     }
+    fun currentMusicModel(): MusicModel? {
+        if(playMusicList.isEmpty()) return null
+
+        return playMusicList[currentPosition]
+    }
 }
 
